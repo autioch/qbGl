1b8aa55d78c91dc56500f1fea88de6e8fb98fd68
Autioch <autioch@gmail.com>
2020-04-04 20:52:46 +0200

    Image processing fixed.

--- src/webglfundamentals.org/03 image processing 04/Scene.js
+++ src/webglfundamentals.org/03 image processing 04/Scene.js
@@ -35,12 +35,12 @@ export default class extends Lib.Scene {
     ];
   }
 
-  render({ context, program }) {
+  render({ context, program, canvas }) {
+    const flipYLocation = program.locateUniform('u_flipY');
+
     this.position.fillBuffer(program.locateAttribute('a_position'));
     this.textCoord.fillBuffer(program.locateAttribute('a_texCoord'));
     context.uniform2f(program.locateUniform('u_textureSize'), this.texture.image.width, this.texture.image.height);
-
-    const flipYLocation = program.locateUniform('u_flipY');
 
     context.uniform1f(flipYLocation, 1); // don't y flip images while drawing to the textures
 
@@ -49,18 +49,18 @@ export default class extends Lib.Scene {
     effectsToApply.forEach((effect, index) => {
       const textFrameBuffer = this.textFrameBuffers[index % 2];
 
-      this.renderTexture(context, program, textFrameBuffer.frameBuffer.frameBuffer, effect);
+      this.renderTexture(context, program, canvas, textFrameBuffer.frameBuffer.frameBuffer, effect);
       context.bindTexture(context.TEXTURE_2D, textFrameBuffer.texture.texture);
     });
     context.uniform1f(flipYLocation, -1);
 
-    this.renderTexture(context, program, null, 'normal');
+    this.renderTexture(context, program, canvas, null, 'normal');
   }
 
-  renderTexture(context, program, frameBuffer, kernelName) {
+  renderTexture(context, program, canvas, frameBuffer, kernelName) { // eslint-disable-line class-methods-use-this
     context.bindFramebuffer(context.FRAMEBUFFER, frameBuffer);
-    context.uniform2f(program.locateUniform('u_resolution'), this.texture.x2, this.texture.y2);
-    context.viewport(0, 0, this.texture.x2, this.texture.y2);
+    context.uniform2f(program.locateUniform('u_resolution'), canvas.width, canvas.height);
+    context.viewport(0, 0, canvas.width, canvas.height);
     context.uniform1fv(program.locateUniform('u_kernel[0]'), kernels[kernelName]);
     context.uniform1f(program.locateUniform('u_kernelWeight'), computeKernelWeight(kernels[kernelName]));
     context.drawArrays(context.TRIANGLES, 0, 6);
